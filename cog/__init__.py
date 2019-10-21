44e1b7dafe44ce7476395ba65530b8866a247a1d
AliyGM <alicia.arenzana@gmail.com>
2019-10-21 14:24:12 +0200

    cogs micro

--- cog/__init__.py
+++ cog/__init__.py
@@ -1,13 +1,43 @@
-from flask import Flask, request, send_file, abort, jsonify
-from rio_tiler import main
-from rio_tiler.utils import array_to_image
-from rio_tiler.profiles import img_profiles
-from rio_tiler.errors import TileOutsideBounds
+import logging
+import os
+import json
 import base64
 import io
 
+from flask import Flask, request, send_file, abort, jsonify
+
+from rio_tiler import main
+from rio_tiler.utils import array_to_image
+from rio_tiler.profiles import img_profiles
+from rio_tiler_mosaic.mosaic import mosaic_tiler
+from rio_tiler_mosaic.methods import defaults
+
+from rio_tiler.errors import TileOutsideBounds
+
+import CTRegisterMicroserviceFlask
+
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(PROJECT_DIR)
+
+#Load swager and register files so we can register them aginst controlTower
+def load_config_json(name):
+    json_path = os.path.abspath(os.path.join(BASE_DIR, f'/microservice/{name}.json'))
+    with open(json_path) as data_file:
+        info = json.load(data_file)
+    return info
+
+# Set App logs
+logging.basicConfig(
+    level=SETTINGS.get('logging', {}).get('level'),
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y%m%d-%H:%M%p',
+
+)
+# App options
 image_options = img_profiles["png"]
 
+# Flask App
 app = Flask(__name__)
 
 @app.route('/cog/<b64url>/<z>/<x>/<y>/',  strict_slashes=False, methods=['GET'])
@@ -15,6 +45,15 @@ def tile(b64url, x, y, z):
     url = str(base64.b64decode(b64url).decode('utf-8'))
     app.logger.debug(f"Getting tile z {z} x {x} y {y} at url {url}")
     try:
+
+
+assets = ["mytif1.tif", "mytif2.tif", "mytif3.tif"]
+tile = (1000, 1000, 9)
+x, y, z = tile
+
+# Use Default First value method
+mosaic_tiler(assets, x, y, z, cogTiler)
+
         tile, mask = main.tile(
             url,
             int(x),
@@ -46,3 +85,42 @@ def metadata(b64url):
         app.logger.debug(e)
         return abort(500)
     return jsonify(metadata)
+
+
+# CT
+info = load_config_json('register')
+swagger = load_config_json('swagger')
+CTRegisterMicroserviceFlask.register(
+    app=app,
+    name='cog-tiles',
+    info=info,
+    swagger=swagger,
+    mode=CTRegisterMicroserviceFlask.AUTOREGISTER_MODE if os.getenv('CT_REGISTER_MODE') and os.getenv('CT_REGISTER_MODE') == 'auto' else CTRegisterMicroserviceFlask.NORMAL_MODE,
+    ct_url=os.getenv('CT_URL'),
+    url=os.getenv('LOCAL_URL')
+)
+
+@app.errorhandler(403)
+def forbidden(e):
+    return error(status=403, detail='Forbidden')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return error(status=404, detail='Not Found (404)')
+
+
+@app.errorhandler(405)
+def method_not_allowed(e):
+    return error(status=405, detail='Method Not Allowed')
+
+
+@app.errorhandler(410)
+def gone(e):
+    return error(status=410, detail='Gone')
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return error(status=500, detail='Internal Server Error')
+
--- cog/middleware.py
+++ cog/middleware.py
@@ -1,5 +1,27 @@
 from flask import request
 from functools import wraps
+import requests
+from CTRegisterMicroserviceFlask import request_to_microservice
+
+class DatasetService(object):
+
+    @staticmethod
+    def execute(config):
+        response = request_to_microservice(config)
+        if not response or response.get('errors'):
+            raise LayerNotFound(message='Layer not found')
+
+        layer = response.get('data', None).get('attributes', None)
+        return layer
+
+    @staticmethod
+    def get(dataset_id):
+        config = {
+            'uri': '/dataset/'+dataset_id,
+            'method': 'GET'
+        }
+        return DatasetService.execute(config)
+
 
 def parse_payload(func):
     """Get payload data"""
@@ -9,3 +31,22 @@ def parse_payload(func):
         kwargs["payload"] = request.args.get('payload', {'payload': None})
         return func(*args, **kwargs)
     return wrapper
+
+
+def get_layer(func):
+    """Get geodata"""
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            if kwargs['map_object'] is None:
+                layer = kwargs['layer']
+                logging.debug('Getting layer ' + layer)
+                kwargs["layer_obj"] = LayerService.get(layer)
+            return func(*args, **kwargs)
+        except LayerNotFound as e:
+            return error(status=404, detail=e.message)
+        except Exception as e:
+            return error(detail='Generic error')
+    return wrapper
+
+
